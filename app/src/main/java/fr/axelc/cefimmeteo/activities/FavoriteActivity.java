7140ec82b0541a041fadb9f7358a4498ec33d6cc
Axel Choteau <axelc37@outlook.fr>
2023-07-24 12:25:56 +0200

    Ajout d'une ville dans la liste

--- app/src/main/java/fr/axelc/cefimmeteo/activities/FavoriteActivity.java
+++ app/src/main/java/fr/axelc/cefimmeteo/activities/FavoriteActivity.java
@@ -1,8 +1,12 @@
 package fr.axelc.cefimmeteo.activities;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import com.google.android.material.snackbar.Snackbar;
@@ -31,8 +35,24 @@ public class FavoriteActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                final AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setTitle(R.string.favorite_add);
+                builder.setMessage(R.string.favorite_add_description);
+                View v = LayoutInflater.from(mContext).inflate(R.layout.dialog_add_favorite, null);
+                final EditText editTextCity = v.findViewById(R.id.favorite_add_edit_text_city);
+                builder.setView(v);
+                builder.setPositiveButton(getString(R.string.favorite_add_confirm), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        mCities.add(new City(editTextCity.getText().toString(), "Pas d'informations, mais il fait probablement chaud", "?°C", R.drawable.weather_sunny_white));
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
+                builder.setNegativeButton(getString(R.string.favorite_add_cancel), (dialogInterface, i) -> {
+
+                });
+
+                builder.create().show();
             }
         });
 
